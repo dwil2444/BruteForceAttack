c7d9db2a9a1b80ab11037c877df3c34b042dffe1
dwil2444 <daneandrew16@gmail.com>
2018-11-19 18:03:12 -0500

    used permutaion function from stack overflow

--- encryption.cpp
+++ encryption.cpp
@@ -1,74 +1,71 @@
 #include <iostream>
+#include<stdio.h> 
+#include<stdlib.h> 
 #include <string>
 #include<crypt.h>
 #include <cstdio>
 #include <ctime>
+#include <vector>
 using namespace std;
 
-std::string randStr(size_t length);
+// function prototypes
+void gen_str(const char*,std::string,const int, const int);
+std::vector<std::string> permutations ;
+int NumberOfPermutations = 0;
+void randStr(size_t length);
 bool decrypt(std::string guess, std::string target);
+int compare (const void * a, const void * b); 
+//////////////////////////////////////////////////////////
+
 string dpass;
 int main()
 {
-
-  std::string ans = randStr(2);
-  //build all possible permutations then add them to a hash map
-  // do a foreach on the map
-  cout << ans << endl;
   string passwd; //original password
   string attempt; //attempts at decrypting password
   string epass; //encrypted password
-  //string dpass; //decrypted password
-  // string alph[52] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", 
-  //                    "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
-  //                     "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d",
-  //                    "e", "f", "g" , "h", "i", "j", "k", "l", "m", "n",
-  //                    "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
-  //                     "y", "z"};
-
-  cout << "Please enter a four letter password: ";
+  cout << "Please enter password to be cracked: ";
   cin >> passwd;
+  int length = passwd.length();
+  char str[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+                'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
+                'e', 'f', 'g' , 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+                'y', 'z'};
+  int n = sizeof str;
+  for (int k = 1; k <= length; k++)
+  {
+    gen_str(str, "", n, k);  
+  }
   epass = crypt(passwd.c_str(), "$1$ab$");
   cout << "Encrypted password: " << epass << endl;
-  bool found = false;
+  std::string* permut_array =  new std::string[NumberOfPermutations];
+  std::copy(permutations.begin(), permutations.end(), permut_array);
   std::clock_t start;
   start = std::clock();
-  double duration;
-  while(!found)
-  {
-    found = decrypt(randStr(passwd.length()+1),epass);
-  }
   std::cout.precision(2);
-  duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
-  cout <<"Found in: " << duration << " seconds\n";
-  cout << dpass;
+  double duration;
+  for (int k = 0; k < NumberOfPermutations; k++)
+  {
+    std:: string test = permut_array[k];
+    if(decrypt(test, epass) == true)
+    {
+      duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+      cout << "The password is: " << dpass << endl;;
+      cout <<"Found in: " << duration << " seconds" << endl;
+      break;
+    }
+  }
   return 0;
 
 }
-std::string randStr(size_t length) 
+void randStr(size_t length) 
 {
-  srand(time(NULL));  //generate a seed by using the current time
-  char str[length];
-  str[length-1] = '\0';
-  size_t i = 0;
-  int r;
-
-  for(i = 0; i < length-1; ++i) 
-  {
-    for(;;) 
-    {
-      r = rand() % 57 + 65; //interval between 65 ('A') and 65+57=122 ('z')
-      if((r >= 65 && r <= 90) || (r >= 97 && r <= 122)) 
-      { // exclude '[' to '`'
-        str[i] = (char)r;
-        break;
-      }
-    }
-  }
-  return string(str);
+  
 }
 bool decrypt(std::string guess, std::string target)
 {
+  //cout << guess;
   if (crypt(guess.c_str(), "$1$ab$") == target)
   {
     dpass = guess.c_str();
@@ -76,23 +73,21 @@ bool decrypt(std::string guess, std::string target)
   }
   return false;
 }
-
-
-
-/*
-To run the code:
-.....................
-
-1-	Upload file.
-2-  ch ..
-3- ls
-4- g++ solution.cpp -lcrypt -o file
-5-./file
-
-
-NOTE that, crypt function is defined only in linux for C++, I am not sure about Python or other languages
-(If you are using windows you can use some online websites such as https://bellard.org/jslinux/vm.html?url=https://bellard.org/jslinux/buildroot-x86.cfg)
-
-
-
-*/
+void gen_str(const char str[],std::string prefix,const int n, const int length)
+{
+    if (length == 1)
+    {
+        for (int j = 0; j < n; j++)
+        {
+          permutations.push_back(prefix + str[j]);
+        }
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            gen_str(str, prefix + str[i], n, length - 1);
+        }
+    }
+    NumberOfPermutations = permutations.size();
+}
